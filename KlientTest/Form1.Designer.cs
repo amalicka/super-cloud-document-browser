1ff3eb11471820613ee2534b3ce94522ba2db8c1
q <i.a.aem@gmail.com>
2015-01-06 20:34:15 +0100

    koniec pracy z mateuszem

--- KlientTest/Form1.Designer.cs
+++ KlientTest/Form1.Designer.cs
@@ -24,11 +24,54 @@
         /// </summary>
         private void InitializeComponent() {
             this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.pictureBox1.Location = new System.Drawing.Point(16, 18);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(433, 460);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(483, 18);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "JestemTestem";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(608, 505);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- KlientTest/Form1.cs
+++ KlientTest/Form1.cs
@@ -7,11 +7,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using KlientTest.ServiceGameReference;
 
 namespace KlientTest {
     public partial class Form1 : Form {
+        Player ja = new Player();
+        Player[] playersi = new Player[0];
+        ServiceGameClient client = new ServiceGameClient();
         public Form1() {
             InitializeComponent();
+            ja.Nick = textBox1.Text;
+            ja.X = 300;
+            ja.Y = 300;
+            timer1.Start();
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e) {
+            foreach (var p in playersi) {
+                e.Graphics.FillEllipse(Brushes.Red, (float)p.X - 8, (float)p.Y - 8, 16, 16);
+                e.Graphics.DrawString(p.Nick, Font, Brushes.Red, (float)p.X + 8, (float)p.Y + 8);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e) {
+            //if (client.State == System.ServiceModel.CommunicationState.Opened ||
+            //    client.State == System.ServiceModel.CommunicationState.Created) {
+                client.SendData(ja);
+                playersi = client.GetData();
+                pictureBox1.Refresh();
+           // }
+
         }
     }
 }
+
+
